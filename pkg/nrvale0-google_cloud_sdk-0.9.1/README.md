ca631260b052b32f8e77f43b99d075ea5b35a946
Nathan R Valentine <nrvale0@gmail.com>
2014-02-05 15:23:48 -0800

    bug fix in install command

---
+++ pkg/nrvale0-google_cloud_sdk-0.9.1/README.md
@@ -0,0 +1,25 @@
+# puppet-google_cloud_sdk
+
+Download and install the Google Cloud SDK tarball on Linux nodes.
+
+## Usage
+    include google_cloud_sdk
+
+## WARNING
+As of 2014.02.04, this module depends on unmerged PR to a dependency:
+* camptocamp/archive (https://github.com/camptocamp/puppet-archive/pull/18)
+
+** Please check the status of this PR before sending bug reports. ** 
+
+## Params
+* install_dir : defaults to /opt/google-cloud-sdk
+
+## Bug reports
+http://github.com/nrvale0/puppet-google_cloud_sdk/issues
+
+## License
+Apache 2.0
+
+## Contact
+Nathan Valentine : <nrvale0@gmail.com> | <Mnathan@puppetlabs.com> | @nrvale0
+
---
+++ pkg/nrvale0-google_cloud_sdk-0.9.1/manifests/init.pp
@@ -0,0 +1,26 @@
+class google_cloud_sdk(
+  $install_dir = $::google_cloud_sdk::params::install_dir
+) inherits google_cloud_sdk::params {
+
+  validate_absolute_path($install_dir)
+
+  File { owner => 'root', group => 'root', mode => '0644', }  
+
+  archive::download { $download_filename: 
+    url => $download_source,
+    checksum => false,
+  }
+
+  archive::extract { 'google-cloud-sdk': 
+    target => $install_dir,
+    require => Archive::Download[$download_filename],
+  }
+
+  exec { 'install Google Cloud SDK':
+    path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+    creates => "${install_dir}/google-cloud-sdk/bin/gcloud",
+    cwd => "${install_dir}/google-cloud-sdk",
+    command => '/bin/echo "" | ./install.sh --usage-reporting false --disable-installation-options --bash-completion false --update-rc false',
+    require => Archive::Extract['google-cloud-sdk'],
+  }
+}
