d5b7c8e8ca9600f1b8f5859cab7bc8543435ab9b
Vladimir Rusinov <vrusinov@google.com>
2014-12-29 14:52:37 +0000

    Remove --update_rc param for Google Cloud SDK installer since current version of installer does not support it.
    Should fix/workaround issue #1.

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,7 +20,7 @@ class google_cloud_sdk(
     path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
     creates => "${install_dir}/google-cloud-sdk/bin/gcloud",
     cwd => "${install_dir}/google-cloud-sdk",
-    command => '/bin/echo "" | ./install.sh --usage-reporting false --disable-installation-options --bash-completion false --update-rc false',
+    command => '/bin/echo "" | ./install.sh --usage-reporting false --disable-installation-options --bash-completion false',
     require => Archive::Extract['google-cloud-sdk'],
   }
 }
